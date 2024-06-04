1cdd699a7c0f6ffaa4b1f978860883dba940697c
Daniel Petrolio <daneil2711@gmail.com>
2024-06-04 00:09:18 -0300

    Ajuste entrypoint hadoop

--- images/hadoop/configs/scripts/bootstrap.sh
+++ images/hadoop/configs/scripts/bootstrap.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+#Validação namenode
+HDFS_METADATA_DIR=$HADOOP_HOME/data/namenode/
+
+if [ "$(ls -A $HDFS_METADATA_DIR)" ]; then
+    echo "NameNode já está formatado."
+else
+    echo "Formatando o NameNode..."
+    hdfs namenode -format
+fi
+
+#Start SSH
 /etc/init.d/ssh start
-hdfs namenode -format
 
 export HDFS_NAMENODE_USER=root
 export HDFS_DATANODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
+export YARN_RESOURCEMANAGER_USER=root
+export YARN_NODEMANAGER_USER=root
 
 # Startando serviços
 $HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
 
 #Inclusão Users
-hdfs dfs -mkdir -p /user/Daniel
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/Daniel
 hdfs dfs -chown -R Daniel:root /user/Daniel
 
+#iniciando jupyter
+jupyter-lab --NotebookApp.token='' --ip='0.0.0.0' --port=8888 --no-browser --allow-root
 while :; do sleep 2073600; done
--- images/jupyter/configs/scripts/bootstrap2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#iniciando o jupyter
-jupyter-lab --NotebookApp.token='' --ip='0.0.0.0' --port=8888 --no-browser --allow-root
-
-while :; do sleep 2073600; done
